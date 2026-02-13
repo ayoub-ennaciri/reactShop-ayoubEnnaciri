c76d11546f9404165227a84919cbaecbd112c532
ayoub <ab.naciri.official@gmail.com>
2026-02-13 10:52:17 +0100

    feat : add stpdbanner to Contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Main } from './modules/main';
 import { Footer } from '../../layouts/footer';
+import Image from "../../assets/images/banners/banner.webp"
+import { StpdBanner } from '../../components/stpdBanner';
 
 export const Contact = () => {
     return (
         <>
+            <StpdBanner text="CONTACT" Image={Image}/>
             <Main/>
             <Footer/>
         </>
